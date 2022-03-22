7b528cc0d6eb08a9d4234e0ed8bf41e08af4205f
Taylor Fahlman <tfahlman@redhat.com>
2022-03-22 16:50:53 -0400

    Bump deps to 4.10 OpenShift release (#40)
    * Bump deps to 4.10 openshift release
    
    * Bump to go 1.17

--- pkg/metrics/service.go
+++ pkg/metrics/service.go
@@ -20,8 +20,8 @@ import (
 	"strconv"
 	"strings"
 
-	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
 	routev1 "github.com/openshift/api/route/v1"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	v1 "k8s.io/api/core/v1"
 	k8serr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
